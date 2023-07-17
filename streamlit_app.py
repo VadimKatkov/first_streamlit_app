aaaa70b2878c2004b560ba3872633aa261172ae9
VadimKatkov <62391897+VadimKatkov@users.noreply.github.com>
2023-07-17 19:41:32 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Bluberry Oatmeal')
+streamlit.text('Kale, Spinacvh & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
